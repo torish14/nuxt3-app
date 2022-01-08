5d99be96763056c4d26f33ef1d8127961206fcec
torish14 <nanamin.104@icloud.com>
2022-01-08 20:13:50 +0900

    ✨ feat: Counterコンポーネントの作成

---
+++ components/Counter.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+    <button @click="increment">+</button>
+    <p>{{ count }}</p>
+    <button @click="decrement">-</button>
+  </div>
+</template>
+
+<script setup lang="ts">
+const title = 'Nuxt3'
+let count = ref(0)
+
+const increment = () => count.value++
+const decrement = () => count.value--
+</script>
