28a8047d7bdaf380cc769d2134f8695899ef179f
torish14 <nanamin.104@icloud.com>
2022-01-09 21:46:59 +0900

    ⛏ chore: pinia, @pinia/nuxt のインストール

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,8 @@ import { defineNuxtConfig } from 'nuxt3'
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
-	typescript: {
-		strict: true
-	}
+  typescript: {
+    strict: true,
+  },
+  buildModules: ['@pinia/nuxt'],
 })
